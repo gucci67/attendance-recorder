e7892f535d9f1df7c11fecb8452b425ecd95d368
gucci67 <w.xig.11cj@gmail.com>
2020-06-02 22:05:58 +0900

    サーバ側にビルド成果物を配置

---
+++ src/main/resources/static/precache-manifest.aaf87191354c5db6c899ece1e5701b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee0a6d37c0a945df6f35581662de749",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8faabb08ce90ae7a88a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f641f3a096ab4734af01",
+    "url": "/static/css/main.bba23342.chunk.css"
+  },
+  {
+    "revision": "b8faabb08ce90ae7a88a",
+    "url": "/static/js/2.2f619c77.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2f619c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f641f3a096ab4734af01",
+    "url": "/static/js/main.d07f7a7a.chunk.js"
+  },
+  {
+    "revision": "9d98af7cd70fc8242878",
+    "url": "/static/js/runtime-main.17b78fd7.js"
+  }
+]);
